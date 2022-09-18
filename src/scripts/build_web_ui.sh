122e3f2c6fa3095fd93542f50bcf8f52537589cd
Brandon Paiz <paiz.brandon@gmail.com>
2022-09-17 21:49:29 -0700

    refactor build_web_ui.sh to speed build times

--- src/scripts/build_web_ui.sh
+++ src/scripts/build_web_ui.sh
@@ -3,14 +3,24 @@
 # cd to dir where this script is held so relative paths work
 cd "$(dirname "$(readlink -f "$0")")"
 
-rm ../RubberNugget/webUI/*.h
-#rm ../RubberNugget/webUI/*.gz
+fileToProgmem() {
+    local file=$1
+    local varname=$2
+
+    # use xxd to convert file to C header format
+    # need to manually replace types, add PROGMEM, etc:w
+    echo "#pragma once"
+    xxd -i "$file" \
+        | sed "s/.*\[\].*/const char $varname\[\] PROGMEM = {/g" \
+        | sed -E "s/unsigned int.*( = .*)/const int ${varname}_len\1/g"
+}
 
 for file in $(find ../RubberNugget/webUI -type f); do
     [ -e "$file" ] || { echo "invalid file $file" && exit 1; }
     fileName=$(basename $file)
     fileName="${fileName%%.*}"
     outFile="$(dirname ${file})/${fileName}.h"
+    varname=${fileName^^}
 
     # TODO: gzip the file first. The below will work but the server needs to add
     # the "Content-Encoding: gzip" header
@@ -18,5 +28,6 @@ for file in $(find ../RubberNugget/webUI -type f); do
     #gzip --keep "$file"
     #./file_to_progmem.sh "$gzipFile" > "$outFile"
 
-    ./file_to_progmem.sh "$file" > "$outFile"
+    echo "converting $file. Importable as '$fileName.h', variable '$varname'"
+    fileToProgmem "$file" "$varname" > "$outFile"
 done
